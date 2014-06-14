2cb5b9514568f0acf6b0d9a8dac0202b9e24fbdc
locuester <dana.hanna@gmail.com>
2014-06-14 08:56:53 -0400

    yeah crazy

--- server/Isda/Isda.Web/Controllers/DeltaOverTimeByLongLatController.cs
+++ server/Isda/Isda.Web/Controllers/DeltaOverTimeByLongLatController.cs
@@ -8,21 +8,17 @@ using System.Web.Http;
 
 namespace Isda.Web.Controllers
 {
-    public class CountAndWeightController : ApiController
+    public class DeltaOverTimeByLongLatController : ApiController
     {
-        public IEnumerable<CountAndWeightByLongLat_Result> Get()
-        {
-            var e = new HackathonEntities();
-            return e.CountAndWeightByLongLat(DateTime.Now - new TimeSpan(14, 0, 0, 0),
-                                            DateTime.Now,
-                                            1, 0, 0, 0, 0);
-        }
-        public IEnumerable<CountAndWeightByLongLat_Result> Get(DateTime start, DateTime end, int precision = 1, 
+
+        public IEnumerable<DeltaOverTimeByLongLat_Result> Get(DateTime start1, DateTime end1, DateTime start2, DateTime end2, int precision = 1, 
                                                                 decimal lat1 = 0, decimal lng1 = 0, decimal lat2 = 0, decimal lng2 = 0)
         {
             var e = new HackathonEntities();
-            return e.CountAndWeightByLongLat(start,
-                                            end,
+            return e.DeltaOverTimeByLongLat(start1,
+                                            end1,
+                                            start2,
+                                            end2,
                                             precision,
                                             lat1,
                                             lng1,
